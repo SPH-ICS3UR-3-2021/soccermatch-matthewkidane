44b8125317226af71557081065e8f46c48413fb2
matthewkidane <matthew.kidane@ocsbstudent.ca>
2021-06-07 01:55:02 -0400

    Create Soccermatch.java

---
+++ .github/Soccermatch/src/soccermatch/Soccermatch.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soccermatch;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author stoov
+ */
+public class Soccermatch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+    }
+    }
+
